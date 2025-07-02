fdf796a0187c65ecca18a92955ffa6cd9583afb1
Krish Doshi <63424106+kridos@users.noreply.github.com>
2025-07-02 01:08:22 +0000

    chapter 3

--- src/Ch03/03_04b/CodeDemo.cpp
+++ src/Ch03/03_04b/CodeDemo.cpp
@@ -11,17 +11,17 @@ int main(){
     std::cout << "Initial Score: " << player_score << std::endl;
     std::cout << "Initial Lives: " << player_lives << std::endl;
 
-    // Postfix increment
-    std::cout << "Score using postfix increment: " << player_score << std::endl;
+    // Postfix increment (updates value after it is used in expression)
+    std::cout << "Score using postfix increment: " << player_score++ << std::endl;
 
-    // Prefix increment
-    std::cout << "Score using prefix increment: " << player_score << std::endl;
+    // Prefix increment (updates value before it is used in expression
+    std::cout << "Score using prefix increment: " << ++player_score << std::endl;
 
-    // Postfix decrement
-    std::cout << "Lives using postfix decrement: " << player_lives << std::endl;
+    // Postfix decrement (updates value after it is used in expression)
+    std::cout << "Lives using postfix decrement: " << player_lives-- << std::endl;
 
-    // Prefix decrement
-    std::cout << "Lives using prefix decrement: " << player_lives << std::endl;
+    // Prefix decrement (updates value before it is used in expression)
+    std::cout << "Lives using prefix decrement: " << --player_lives << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -2,17 +2,41 @@
 // Exercise 03_06
 // Preprocessor Directives, by Eduardo Corpeño 
 
+//All directives start with a '#' and occupy one line each (Ex: #include)
 #include <iostream>
 #include <cstdint>
 
+//Defines symbol to be whatever is typed to the right of the symbol (find and replace) but usually used for constants (can also use const keyword to define regular variables as constants)
+#define MAX_AMMO 500
+#define DEBUG
+
 int main(){
-    int32_t ammo = 100;
+
+    //This type of formatting is portable across architectures
+
+    //no 'u' so signed
+    //then type (int)
+    //then amount of bits (32)
+    //then _t for type
+    int32_t ammo = MAX_AMMO / 5;
+
+    //'u' for unsigned
+    //then type (int)
+    //then amount of bits (32)
+    //then _t for type
     uint8_t health_items = 5;
+
+//Can use to run code if macro (DEBUG) is defined
+#ifdef DEBUG
+    std::cout << "[DEBUG] Starting game simulation" << std:: endl;
+#endif
 
     ammo += 200; // Player finds extra ammo
     health_items -= 2; // Player uses some health items
 
     std::cout << "Final Ammo: " << ammo << std::endl;
+
+    //Need to explicitly cast to int or else it would be interpretted as text character
     std::cout << "Remaining Health Items: " << (int) health_items << std::endl;
 
     std::cout << std::endl << std::endl;
